bbe4696514f2d06e260c75c16c7d13584fd23c52
AshmiyaV <ashmiyavs6767@gmail.com>
2024-02-07 01:46:01 -0500

    [Assignment 2] - user apis

--- src/main/java/com/neu/csye6225/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.neu.csye6225.repository;
-
-import com.neu.csye6225.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
