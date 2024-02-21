9881bf86a00af7992027602130b3ec6eb4b73883
AshmiyaV <ashmiyavs6767@gmail.com>
2024-02-21 01:34:33 -0500

    [Assignment-4] installation script

---
+++ packer/script/installation.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+#Install Java 
+sudo dnf install java-17-openjdk-devel.x86_64 -y
+
+#Install mySQL
+sudo dnf install mysql-server -y
+
+#Start mySQL
+sudo systemctl start mysqld
+
+#Alter Password of mySQL
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Password#1';"
