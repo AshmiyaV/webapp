40c3cfca8cbf1a113acb63c9a1141aa2da441a78
AshmiyaV <ashmiyavs6767@gmail.com>
2024-02-21 05:27:53 -0500

    [Assignment-4] group in user script

--- packer/script/user.sh
+++ packer/script/user.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 #Creating group csye6225
+sudo groupadd csye6225
 
 #Creating user csye6225
-sudo adduser csye6225 --shell /usr/sbin/nologin
+sudo adduser csye6225 --shell /usr/sbin/nologin -g csye6225
