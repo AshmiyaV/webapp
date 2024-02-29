234b90720bc1cafd0364deeb6bfda6d9f9c92d09
AshmiyaV <ashmiyavs6767@gmail.com>
2024-02-29 14:36:35 -0500

    [Assignment-5] webapp changes

--- packer/script/chown.sh
+++ packer/script/chown.sh
@@ -2,3 +2,5 @@
 
 #Changing ownership of the JAR file
 sudo chown -R csye6225:csye6225 /tmp/webapp-1.0-SNAPSHOT.jar
+
+sudo mv /tmp/webapp-1.0-SNAPSHOT.jar /opt/webapp-1.0-SNAPSHOT.jar
--- packer/script/installation.sh
+++ packer/script/installation.sh
@@ -4,14 +4,14 @@
 sudo dnf install java-17-openjdk-devel.x86_64 -y
 
 #Install mySQL
-sudo dnf install mysql mysql-server -y
-sleep 5
+# sudo dnf install mysql mysql-server -y
+# sleep 5
 
 #Start mySQL
-sudo systemctl enable mysqld
-sudo systemctl start mysqld
+# sudo systemctl enable mysqld
+# sudo systemctl start mysqld
 
-sleep 5
+# sleep 5
 
 #Alter Password of mySQL
-sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
+# sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
