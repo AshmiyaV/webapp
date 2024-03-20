cd6a4824ac1890266146890e51eed87a78e9c0bd
AshmiyaV <ashmiyavs6767@gmail.com>
2024-03-20 19:47:19 -0400

    connection checker logs

--- src/main/java/com/neu/csye6225/controller/ConnectionCheckerController.java
+++ src/main/java/com/neu/csye6225/controller/ConnectionCheckerController.java
@@ -1,6 +1,8 @@
 package com.neu.csye6225.controller;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -24,19 +26,23 @@ public class ConnectionCheckerController {
 		httpHeaders.setCacheControl(CacheControl.noCache().mustRevalidate());
 		httpHeaders.setPragma("no-cache");
 		httpHeaders.add("X-Content-Type-Options", "nosniff");
+		final Logger logger = LoggerFactory.getLogger(ConnectionCheckerController.class);
 		try {
 			if((requestParam != null && !requestParam.isEmpty()) || (requestBody != null && !requestBody.isEmpty()) || auth != null) {
+				logger.warn("Request param or Body is present in GET Request");
 				return ResponseEntity
 						.status(HttpStatus.BAD_REQUEST)
 						.headers(httpHeaders)
 						.build();
 			}
 		connectioncheckerService.checkDatabaseConnection();
+			logger.info("The request is successful!");
 		return ResponseEntity
 				.status(HttpStatus.OK)
 				.headers(httpHeaders)
 				.build();
 		} catch(RuntimeException e) {
+			logger.error("The service is unavailable");
 			return ResponseEntity
 					.status(HttpStatus.SERVICE_UNAVAILABLE)
 					.headers(httpHeaders)
@@ -46,10 +52,12 @@ public class ConnectionCheckerController {
 
 	@RequestMapping(method = {RequestMethod.OPTIONS, RequestMethod.HEAD})
 	public ResponseEntity<Void> handleCheckDatabaseHeadAndOptionsMethod() {
+		final Logger logger = LoggerFactory.getLogger(ConnectionCheckerController.class);
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.setCacheControl(CacheControl.noCache().mustRevalidate());
 		httpHeaders.setPragma("no-cache");
 		httpHeaders.add("X-Content-Type-Options", "nosniff");
+		logger.error("The method is not allowed.");
 		return ResponseEntity
 				.status(HttpStatus.METHOD_NOT_ALLOWED)
 				.headers(httpHeaders)
