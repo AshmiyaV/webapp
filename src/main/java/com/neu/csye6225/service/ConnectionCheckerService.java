0453779dbd748a45fd8bad53431489bfd1a4396f
AshmiyaV <ashmiyavs6767@gmail.com>
2024-03-20 19:47:45 -0400

    connection checker service logs

--- src/main/java/com/neu/csye6225/service/ConnectionCheckerService.java
+++ src/main/java/com/neu/csye6225/service/ConnectionCheckerService.java
@@ -3,12 +3,13 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConnectionCheckerService {
-	
 	private final DataSource dataSource;
 
 	@Autowired
@@ -18,8 +19,10 @@ public class ConnectionCheckerService {
 	}
 	
 	public void checkDatabaseConnection() {
+		final Logger logger = LoggerFactory.getLogger(ConnectionCheckerService.class);
 		try {
 			dataSource.getConnection();
+			logger.debug("Health Check Connection Successful!!");
 			System.out.println("Health Check Connection Successful!!");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
